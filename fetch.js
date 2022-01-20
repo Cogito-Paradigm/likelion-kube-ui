ce32f0c8ab82c77c79a43c645ba9de6c14ada9f3
ichangho <ch.lee@rootenergy.co.kr>
2022-01-21 00:25:30 +0900

    fix fetch uri

--- fetch.js
+++ fetch.js
@@ -1,8 +1,65 @@
+const movies = [
+	{
+		id: 1,
+		name: 'evil-dead-2013',
+	},
+	{
+		id: 2,
+		name: 'annabelle-creation',
+	},
+	{
+		id: 3,
+		name: 'insidious-the-last-key',
+	},
+	{
+		id: 4,
+		name: 'the-conjuring-2',
+	},
+	{
+		id: 5,
+		name: 'dawn-of-justice',
+	},
+	{
+		id: 6,
+		name: 'suicide-squad',
+	},
+	{
+		id: 7,
+		name: 'john-wick-chapter-1',
+	},
+	{
+		id: 8,
+		name: 'john-wick-chapter-2',
+	},
+	{
+		id: 9,
+		name: 'john-wick-chapter-3',
+	},
+	{
+		id: 10,
+		name: 'lost-in-space-s1',
+	},
+	{
+		id: 11,
+		name: 'lost-in-space-s2',
+	},
+	{
+		id: 12,
+		name: 'pacific-rim-2013',
+	},
+	{
+		id: 13,
+		name: 'pacific-rim-uprising-2017',
+	},
+];
+//
+
 window.onload = function () {
 	var URLSearch = new URLSearchParams(location.search);
 	var id = URLSearch.get('id');
+	var { name: moviename } = movies.filter((data) => data.id == id)[0];
 	//
-	fetch(`${location.origin}/api/detail?id=${id}`)
+	fetch(`${location.origin}/api/movie/${moviename}`)
 		.then((response) => response.json())
 		.then((data) => {
 			//
